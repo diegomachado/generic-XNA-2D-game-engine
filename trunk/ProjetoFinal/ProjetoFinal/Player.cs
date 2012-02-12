78ecc6c35b734c44e5692e1db02028bfb9f763d1
diegomac@gmail.com <diegomac@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-02-12 20:26:33 +0000

    Ajeitando bugs que tive com o SVN.

---
+++ trunk/ProjetoFinal/ProjetoFinal/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ProjetoFinal
+{
+    class Player
+    {
+        public Texture2D PlayerSkin;
+
+        public Vector2 Position;
+
+        public int Width
+        {
+            get { return PlayerSkin.Width; }
+        }
+
+        public int Height
+        {
+            get { return PlayerSkin.Height; }
+        }
+
+
+        public void Initialize(Texture2D skin, Vector2 position)
+        {
+            PlayerSkin = skin;            
+            Position = position;
+        }
+
+        public void Update()
+        {
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(PlayerSkin, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+        } 
+    }
+}
