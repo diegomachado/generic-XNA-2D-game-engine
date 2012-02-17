1c31530123769ff1661873f675279c28722ea23a
diegomac@gmail.com <diegomac@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-02-17 13:16:55 +0000

    Troca de mensagens funcionando perfeitamente. Tudo registrado por Debug via Console.

--- trunk/ProjetoFinal/ProjetoFinal/Game1.cs
+++ trunk/ProjetoFinal/ProjetoFinal/Game1.cs
@@ -10,29 +10,47 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
 
+using Lidgren.Network;
+
+//Debug
+using System.Diagnostics;
+
 namespace ProjetoFinal
 {
+
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
         Player player;
+        Dictionary<long,Player> otherPlayers;
+
         KeyboardState currentKeyboardState;
         KeyboardState previousKeyboardState;
 
         GamePadState currentGamePadState;
         GamePadState previousGamePadState;
 
+        NetClient client;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
+
+            NetPeerConfiguration config = new NetPeerConfiguration("projetofinal");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+
+            client = new NetClient(config);
+            client.Start();
         }
 
         protected override void Initialize()
         {
-            player = new Player();
+            otherPlayers = new Dictionary<long,Player>();
+
+            client.DiscoverLocalPeers(666);
 
             base.Initialize();
         }
@@ -45,7 +63,9 @@ namespace ProjetoFinal
                                                  GraphicsDevice.Viewport.TitleSafeArea.Y
                                                     + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
 
-            player.Initialize(Content.Load<Texture2D>("sprites/bear"), playerPosition, 8.0f );
+            Console.WriteLine("Entre com seu nick:");
+            string nick = Console.ReadLine();
+            player = new Player(nick, Content.Load<Texture2D>("sprites/bear"), playerPosition, 8.0f );
         }
 
         protected override void UnloadContent()
@@ -73,6 +93,9 @@ namespace ProjetoFinal
         {
             Vector2 playerPositionTemp = player.position;
 
+            float prevXPosition = player.position.X;
+            float prevYPosition = player.position.Y;
+
             player.position.X += currentGamePadState.ThumbSticks.Left.X * player.speed;
             player.position.Y -= currentGamePadState.ThumbSticks.Left.Y * player.speed;
 
@@ -87,6 +110,47 @@ namespace ProjetoFinal
 
             player.position.X = MathHelper.Clamp(player.position.X,0, GraphicsDevice.Viewport.Width - player.Width);
             player.position.Y = MathHelper.Clamp(player.position.Y,0, GraphicsDevice.Viewport.Height - player.Height);
+
+            // Cliente envia atualizações pro Servidor
+            if (prevXPosition != player.position.X || prevYPosition != player.position.Y)
+            {
+                NetOutgoingMessage om = client.CreateMessage();
+
+                om.Write(player.position.X);
+                om.Write(player.position.Y);
+                om.Write(player.nickname);
+
+                client.SendMessage(om, NetDeliveryMethod.Unreliable);
+            }
+
+            // Cliente recebe atualizações do Servidor          
+            NetIncomingMessage im;
+            while ((im = client.ReadMessage()) != null)
+            {
+                switch (im.MessageType)
+                {
+                    case NetIncomingMessageType.DiscoveryResponse:
+                        client.Connect(im.SenderEndpoint);
+                        break;
+
+                    case NetIncomingMessageType.Data:
+                        long id = im.ReadInt64();
+                        //string nick = im.ReadString();
+                        float x = im.ReadFloat();
+                        float y = im.ReadFloat();
+
+                        Console.WriteLine(id + " online; X = " + x + "; Y = " + y);
+                        //Console.WriteLine(nick + " online; X = " + x + "; Y = " + y );
+
+                        //if (!otherPlayers.ContainsKey(id))
+                        //    otherPlayers[id] = new Player(Content.Load<Texture2D>("sprites/bear"), new Vector2(x, y), 8.0f);
+                        //else
+                        //    otherPlayers[id].position = new Vector2(x, y);
+
+                        break;
+                }
+            }
+            
         }
 
         protected override void Draw(GameTime gameTime)
@@ -95,9 +159,22 @@ namespace ProjetoFinal
             
             spriteBatch.Begin();
             player.Draw(spriteBatch);
+
+            foreach (var p in otherPlayers)
+                p.Value.Draw(spriteBatch);
+
             spriteBatch.End();            
 
             base.Draw(gameTime);
         }
+
+        // Desconecta o cliente do servidor ao fechar o jogo
+        protected override void OnExiting(object sender, EventArgs args)
+        {
+            client.Shutdown("bye");
+
+            base.OnExiting(sender, args);
+        }
     }
 }
+
--- trunk/ProjetoFinal/ProjetoFinal/Player.cs
+++ trunk/ProjetoFinal/ProjetoFinal/Player.cs
@@ -9,6 +9,22 @@ namespace ProjetoFinal
         public Texture2D skin;
         public Vector2 position;
         public float speed;
+        public string nickname;
+
+        public Player(Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
+        {
+            this.skin = playerSkin;
+            this.position = playerPosition;
+            this.speed = playerSpeed;
+        }
+
+        public Player(string playerNickname, Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
+        {
+            this.nickname = playerNickname;
+            this.skin = playerSkin;
+            this.position = playerPosition;
+            this.speed = playerSpeed;
+        }
 
         public int Width
         {
@@ -18,7 +34,7 @@ namespace ProjetoFinal
         public int Height
         {
             get { return skin.Height; }
-        }
+        }        
 
         public void Initialize(Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
         {
