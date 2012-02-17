a14334e39cb93f66d501d9c08bd6a6e6012cc41b
diegomac@gmail.com <diegomac@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-02-17 13:16:55 +0000

    Troca de mensagens funcionando perfeitamente. Tudo registrado por Debug via Console.

--- ProjetoFinal/XnaGameServer/Program.cs
+++ ProjetoFinal/XnaGameServer/Program.cs
@@ -9,114 +9,132 @@ namespace XnaGameServer
 	{
 		static void Main(string[] args)
 		{
-			NetPeerConfiguration config = new NetPeerConfiguration("xnaapp");
+            NetPeerConfiguration config = new NetPeerConfiguration("projetofinal");
 			config.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
-			config.Port = 14242;
+			config.Port = 666;
 
-			// create and start server
+			// Cria e inicia o Servidor
 			NetServer server = new NetServer(config);
 			server.Start();
 
-			// schedule initial sending of position updates
-			double nextSendUpdates = NetTime.Now;
+			// Agenda o envio de atualizações
+            double nextSendUpdates = NetTime.Now;
 
-			// run until escape is pressed
 			while (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Escape)
 			{
-				NetIncomingMessage msg;
-				while ((msg = server.ReadMessage()) != null)
+                // Mensagem de algum cliente para o Servidor
+                NetIncomingMessage im ;
+
+                while ((im = server.ReadMessage()) != null)
 				{
-					switch (msg.MessageType)
+                    switch (im.MessageType)
 					{
-						case NetIncomingMessageType.DiscoveryRequest:
-							//
-							// Server received a discovery request from a client; send a discovery response (with no extra data attached)
-							//
-							server.SendDiscoveryResponse(null, msg.SenderEndpoint);
+						// Faz o Hand-Shake inicial com o cliente
+                        case NetIncomingMessageType.DiscoveryRequest:
+                            server.SendDiscoveryResponse(null, im.SenderEndpoint);
 							break;
+
 						case NetIncomingMessageType.VerboseDebugMessage:
 						case NetIncomingMessageType.DebugMessage:
 						case NetIncomingMessageType.WarningMessage:
-						case NetIncomingMessageType.ErrorMessage:
-							//
-							// Just print diagnostic messages to console
-							//
-							Console.WriteLine(msg.ReadString());
-							break;
+						
+                        case NetIncomingMessageType.ErrorMessage:
+                            Console.WriteLine(im.ReadString());
+                            break;
+
 						case NetIncomingMessageType.StatusChanged:
-							NetConnectionStatus status = (NetConnectionStatus)msg.ReadByte();
-							if (status == NetConnectionStatus.Connected)
-							{
-								//
-								// A new player just connected!
-								//
-								Console.WriteLine(NetUtility.ToHexString(msg.SenderConnection.RemoteUniqueIdentifier) + " connected!");
-
-								// randomize his position and store in connection tag
-								msg.SenderConnection.Tag = new int[] {
-									NetRandom.Instance.Next(10, 100),
-									NetRandom.Instance.Next(10, 100)
-								};
-							}
+                            NetConnectionStatus status = (NetConnectionStatus)im.ReadByte();
+							
+                            // Um novo jogador se conectou
+                            if (status == NetConnectionStatus.Connected)
+                                Console.WriteLine("Cliente " + NetUtility.ToHexString(im.SenderConnection.RemoteUniqueIdentifier) + " conectado.");
 
 							break;
+
+                        // Chegou input através de algum cliente
 						case NetIncomingMessageType.Data:
-							//
-							// The client sent input to the server
-							//
-							int xinput = msg.ReadInt32();
-							int yinput = msg.ReadInt32();
+							float xinput = im.ReadFloat();
+                            float yinput = im.ReadFloat();
+                            string nick = im.ReadString();
 
-							int[] pos = msg.SenderConnection.Tag as int[];
+                            Console.WriteLine("Nick: " + nick + " | X: " + xinput + " | Y: " + yinput);
 
-							// fancy movement logic goes here; we just append input to position
-							pos[0] += xinput;
-							pos[1] += yinput;
+                            //ClientInfo ci = im.SenderConnection.Tag as ClientInfo;
+                            //ci = new ClientInfo(nick, xinput, yinput);			
+                            float[] pos = im.SenderConnection.Tag as float[];
+                            pos[0] = xinput;
+                            pos[1] = yinput;
+                            
 							break;
 					}
 
-					//
-					// send position updates 30 times per second
-					//
+                    // Envia atualizações 30x/s
 					double now = NetTime.Now;
 					if (now > nextSendUpdates)
 					{
-						// Yes, it's time to send position updates
-
-						// for each player...
+						// Para cada jogador...
 						foreach (NetConnection player in server.Connections)
 						{
-							// ... send information about every other player (actually including self)
+							// ... envio suas informações para os outros jogadores (incluindo a si mesmo)
 							foreach (NetConnection otherPlayer in server.Connections)
 							{
-								// send position update about 'otherPlayer' to 'player'
-								NetOutgoingMessage om = server.CreateMessage();
+                                NetOutgoingMessage om = server.CreateMessage();
 
-								// write who this position is for
+								// Escreve de quem se trata essa posição
+                                // RemoteUniqueIdentifier é um long, que representa o id do Cliente
 								om.Write(otherPlayer.RemoteUniqueIdentifier);
 
-								if (otherPlayer.Tag == null)
-									otherPlayer.Tag = new int[2];
+                                if (otherPlayer.Tag == null)
+                                    otherPlayer.Tag = new float[2];
 
-								int[] pos = otherPlayer.Tag as int[];
-								om.Write(pos[0]);
-								om.Write(pos[1]);
+                                float[] pos = otherPlayer.Tag as float[];
+                                om.Write(pos[0]);
+                                om.Write(pos[1]);
+                                
+                                // Ainda preciso entender por que criar um objeto vazio caso seja null.
+                                //if (otherPlayer.Tag == null)
+                                    //otherPlayer.Tag = new ClientInfo();
 
-								// send message
+                                // Obtendo através da Tag os dados gravados do cliente 
+                                // assim que ele enviou uma mensagem
+                                //ClientInfo ci = otherPlayer.Tag as ClientInfo;
+                                //om.Write(ci.nickname);
+                                //om.Write(ci.xPosition);
+                                //om.Write(ci.yPosition);                                			
+                                
+								// Envia mensagem
 								server.SendMessage(om, player, NetDeliveryMethod.Unreliable);
 							}
 						}
 
-						// schedule next update
+                        // Agenda a próxima atualização
 						nextSendUpdates += (1.0 / 30.0);
 					}
 				}
 
-				// sleep to allow other processes to run smoothly
+                // Sleep para que outros processos rodem na moral
 				Thread.Sleep(1);
 			}
 
-			server.Shutdown("app exiting");
+			server.Shutdown("Desligando Servidor.");
 		}
 	}
+
+    class ClientInfo
+    {
+        public float xPosition { get; set; }
+        public float yPosition { get; set; }
+        public string nickname { get; set; }
+
+        public ClientInfo() { }
+
+        public ClientInfo(string nick, float xPos, float yPos)
+        {
+            this.nickname = nick;
+            this.xPosition = xPos;
+            this.yPosition = yPos;
+        }
+
+    }
+
 }
