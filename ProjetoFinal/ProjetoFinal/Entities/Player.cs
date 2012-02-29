78907f32f607b79871eb8dcb499a3033fdd3b930
guifes@gmail.com <guifes@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-02-29 15:57:10 +0000

    Adcionando Player a Tela

--- ProjetoFinal/ProjetoFinal/Entities/Player.cs
+++ ProjetoFinal/ProjetoFinal/Entities/Player.cs
@@ -1,55 +1,14 @@
 ﻿using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 
 namespace ProjetoFinal.Entities
 {
     class Player
     {
-        public Texture2D skin;
-        public Vector2 position;
-        public float speed;
         public string nickname;
 
-        public Player(Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
-        {
-            this.skin = playerSkin;
-            this.position = playerPosition;
-            this.speed = playerSpeed;
-        }
-
-        public Player(string playerNickname, Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
+        public Player(string playerNickname)
         {
             this.nickname = playerNickname;
-            this.skin = playerSkin;
-            this.position = playerPosition;
-            this.speed = playerSpeed;
         }
-
-        public int Width
-        {
-            get { return skin.Width; }
-        }
-
-        public int Height
-        {
-            get { return skin.Height; }
-        }        
-
-        public void Initialize(Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
-        {
-            skin = playerSkin;
-            position = playerPosition;
-            speed = playerSpeed;
-        }
-
-        public void Update()
-        {
-        }
-
-        public void Draw(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(skin, position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-        } 
     }
 }
---
+++ ProjetoFinal/ProjetoFinal/Entities/PlayerChar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ProjetoFinal.Entities
+{
+    class PlayerChar : Char
+    {
+        public PlayerChar(Texture2D playerSkin, Vector2 playerPosition, float playerSpeed)
+            : base(playerSkin, playerPosition, playerSpeed)
+        {
+            this.skin = playerSkin;
+            this.position = playerPosition;
+            this.speed = playerSpeed;
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState,  Rectangle clientBounds)
+        {
+            Vector2 inputDirection = Vector2.Zero;
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+                inputDirection.X -= 1;
+            if (keyboardState.IsKeyDown(Keys.Right))
+                inputDirection.X += 1;
+            if (keyboardState.IsKeyDown(Keys.Up))
+                inputDirection.Y -= 1;
+            if (keyboardState.IsKeyDown(Keys.Down))
+                inputDirection.Y += 1;
+
+            position += inputDirection * speed;
+            position.X = MathHelper.Clamp(position.X, 0, clientBounds.Width - Width);
+            position.Y = MathHelper.Clamp(position.Y, 0, clientBounds.Height - Height);
+
+            base.Update();
+        }
+    }
+}
