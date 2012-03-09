4208bb276e565206307d35cf8c245163157ec10d
diegomac@gmail.com <diegomac@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-03-09 03:42:20 +0000

    Mil coisas. Lots of shit.

---
+++ trunk/ProjetoFinal/ProjetoFinal/EventArgs/PlayerStateChangedArgs.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoFinal.Entities;
+
+namespace ProjetoFinal.EventArgs
+{
+    using System;
+
+    class PlayerStateChangedArgs : EventArgs
+    {
+        public Player player { get; set; }
+
+        public PlayerStateChangedArgs(Player player)
+        {
+            this.player = player;
+        }
+    }
+}
