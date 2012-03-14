2d82fe47be5007c833c52fbb115b5f1e599c7a79
guifes@gmail.com <guifes@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-03-14 02:32:56 +0000

    Refactoring na arquitetura

--- ProjetoFinal/ProjetoFinal/Managers/LocalPlayerManager.cs
+++ ProjetoFinal/ProjetoFinal/Managers/LocalPlayerManager.cs
@@ -14,44 +14,57 @@ namespace ProjetoFinal.Managers
 {
     class LocalPlayerManager
     {
+        private short playerId;
         public event EventHandler<PlayerStateChangedArgs> PlayerStateChanged;
         private Player localPlayer;
 
         public LocalPlayerManager()
         {
-            localPlayer = new Player(0, TextureManager.Instance.getTexture(TextureList.Bear), Vector2.Zero);
+        }
+
+        public void createLocalPlayer(short id)
+        {
+            playerId = id;
+            localPlayer = new Player(TextureManager.Instance.getTexture(TextureList.Bear), Vector2.Zero);        
+        }
+        
+        protected void OnPlayerStateChanged()
+        {
+            if (PlayerStateChanged != null)
+                PlayerStateChanged(this, new PlayerStateChangedArgs(playerId, localPlayer));
         }
 
         public void Update(GameTime gameTime, KeyboardState keyboardState, GamePadState gamePadState, Rectangle clientBounds)
         {
-            Vector2 inputDirection = Vector2.Zero;
+            if (localPlayer != null)
+            {
+                Vector2 inputDirection = Vector2.Zero;
 
-            if (keyboardState.IsKeyDown(Keys.Left))
-                inputDirection.X -= 1;
-            if (keyboardState.IsKeyDown(Keys.Right))
-                inputDirection.X += 1;
-            if (keyboardState.IsKeyDown(Keys.Up))
-                inputDirection.Y -= 1;
-            if (keyboardState.IsKeyDown(Keys.Down))
-                inputDirection.Y += 1;
+                if (keyboardState.IsKeyDown(Keys.Left))
+                    inputDirection.X -= 1;
+                if (keyboardState.IsKeyDown(Keys.Right))
+                    inputDirection.X += 1;
+                if (keyboardState.IsKeyDown(Keys.Up))
+                    inputDirection.Y -= 1;
+                if (keyboardState.IsKeyDown(Keys.Down))
+                    inputDirection.Y += 1;
 
-            localPlayer.position += inputDirection * localPlayer.speed;
-            localPlayer.position = new Vector2(MathHelper.Clamp(localPlayer.position.X, 0, clientBounds.Width - localPlayer.Width),
-                                               MathHelper.Clamp(localPlayer.position.Y, 0, clientBounds.Height - localPlayer.Height));
+                localPlayer.position += inputDirection * localPlayer.speed;
+                localPlayer.position = new Vector2(MathHelper.Clamp(localPlayer.position.X, 0, clientBounds.Width - localPlayer.Width),
+                                                   MathHelper.Clamp(localPlayer.position.Y, 0, clientBounds.Height - localPlayer.Height));
 
-            // TODO: Dar um jeito de mandar menos mensagens
-            OnPlayerStateChanged();
+                // TODO: Dar um jeito de mandar menos mensagens
+                OnPlayerStateChanged();
+            }
         }
 
-        public void Draw(SpriteBatch spriteBatch)
+        public void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont)
         {
-            localPlayer.Draw(spriteBatch);
-        }
-
-        protected void OnPlayerStateChanged()
-        {
-            if (PlayerStateChanged != null)
-                PlayerStateChanged(localPlayer, new PlayerStateChangedArgs(localPlayer));
+            if (localPlayer != null)
+            {
+                localPlayer.Draw(spriteBatch);
+                spriteBatch.DrawString(spriteFont, playerId.ToString(), localPlayer.position, Color.White);
+            }
         }
     }
 }
