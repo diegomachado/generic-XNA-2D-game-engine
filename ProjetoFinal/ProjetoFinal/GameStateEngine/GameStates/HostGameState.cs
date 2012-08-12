b3d0032d6e9742990bfeacc8b84acb2dfe753c0d
guifes@gmail.com <guifes@gmail.com@b125a65b-3739-5f72-c460-350bd151032f>
2012-08-12 19:08:49 +0000

    TextField

--- ProjetoFinal/ProjetoFinal/GameStateEngine/GameStates/HostGameState.cs
+++ ProjetoFinal/ProjetoFinal/GameStateEngine/GameStates/HostGameState.cs
@@ -16,6 +16,8 @@ namespace ProjetoFinal.GameStateEngine.GameStates
             guiManager.AddElement(new Button("Start Server", new Rectangle(100, 100, 305, 51), textureManager.getTexture(TextureList.ButtonFrame), OnStartServerButtonPressed));
 
             guiManager.AddElement(new Button("Main Menu", new Rectangle(100, 200, 305, 51), textureManager.getTexture(TextureList.ButtonFrame), OnMainMenuButtonPressed));
+
+            guiManager.AddElement(new TextField("127.0.0.1", new Rectangle(100, 300, 305, 51), textureManager.getTexture(TextureList.ButtonFrame)));
         }
 
         public override void Update(Managers.InputManager inputManager, GameTime gameTime)
